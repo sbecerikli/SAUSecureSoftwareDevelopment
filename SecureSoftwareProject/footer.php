73ed6f25abe335347e9b172c542f14b1ea0f2be0
Seyfullah Becerikli <sbecerikli@gmail.com>
2021-05-15 22:22:40 +0300

    Delete SecureSoftwareProject directory

--- SecureSoftwareProject/footer.php
+++
@@ -1,8 +0,0 @@
-<!--jQuery Library-->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<!--Popper.JS-->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-<!--Bootstrap JS-->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-</body>
-</html>
